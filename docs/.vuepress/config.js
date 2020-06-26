69c936a98f0542f4de93423fe87b4f55311714ca
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2020-06-26 18:47:20 -0500

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,75 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'Node.js',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+  base: '/node-docs/',
+
+  /**
+   * AntDocs theme for AntDesign for vue
+   * Ref: https://antdocs.seeyoz.cn/guide/getting-started.html#引用主题
+   */
+  theme: 'antdocs',
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    [
+      'meta',
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }
+    ]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: 'https://github.com/rnovec',
+    editLinks: false,
+    logo: 'https://pablomagaz.com/content/images/2020/01/NodeJs.png',
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: 'Intro',
+        link: '/intro/'
+      },
+      {
+        text: 'Evaluacion',
+        link: '/exam/'
+      },
+      {
+        text: 'Glosario',
+        link: '/glossary/'
+      }
+    ],
+    sidebar: {
+      '/intro/': [
+        {
+          title: 'Intro',
+          collapsable: false,
+          children: ['', 'base-de-datos', 'tdd']
+        }
+      ]
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: ['@vuepress/plugin-back-to-top', '@vuepress/plugin-medium-zoom']
+}
---
+++ docs/README.md
@@ -0,0 +1,42 @@
+---
+home: true
+heroImage: https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg
+tagline:
+actionText: Quick Start →
+actionLink: /intro/
+footer: Made by rnovec with ❤️
+---
+
+# Introducción
+Node.js® es un entorno de ejecución para JavaScript construido con el [motor de JavaScript V8 de Chrome](https://v8.dev/).
+
+# Objetivos de aprendizaje
+
+- Aprender la síntaxis más moderna de **JS** y **ECMAScript 2019**.
+- Conocer el entorno de desarrollo de **Node.js**.
+- Conocer e implementar los conceptos alrededor del diseño de una **API REST** en Node.js.
+
+# Evaluación
+
+- El alumno contestará un test de conocimientos generales de **Node.js** y **API RESTful** con el fin de evaluar su apredizaje y reforzar sus conocimientos.
+
+# Actividades
+
+- Crear un proyecto simple con **Express Generator** y evaluar su estructura.
+- Conectarnos a una base de datos **PostgreSQL**.
+- Implementar un desarrollo basado en pruebas (**TDD**).
+- Contruir una API REST y probar los endpoints en **POSTMAN**.
+
+# Recursos del Taller
+
+- [Documentación Oficial](https://nodejs.org/es/)
+- [Motor V8 Chrome](https://v8.dev/)
+- [Express](https://www.npmjs.com/package/express)
+- [Express Generator](https://www.npmjs.com/package/express-generator/)
+- [Node PG](https://node-postgres.com/)
+- [VSCode](https://code.visualstudio.com/download)
+
+# Instructor
+
+<avatar>
+</avatar>
